67e44b9d3c6ed6b5344d8e359fdf884c0d6f698c
nicopys42 <123968243+nicopys42@users.noreply.github.com>
2025-05-12 14:04:39 +0530

    Update auth.middleware.js

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -14,7 +14,7 @@ const protectRoute = async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         //find user
-        const user = await User.findById(decode.userId).select("-password");
+        const user = await User.findById(decoded.userId).select("-password");
         if(!user) return res.status(401).json({ message: "Token is not valid" });
         req.user = user;
         next();
