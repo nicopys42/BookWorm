e1960e7a98ab6b5df8c1014e08e254e87753a076
nicopys42 <123968243+nicopys42@users.noreply.github.com>
2025-05-16 13:55:57 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -55,7 +55,8 @@ router.post("/register", async (req, res) => {
                 id: user._id,
                 username: user.username,
                 email:user.email,
-                profileImage: user.profileImage
+                profileImage: user.profileImage,
+                createdAt: user.createdAt,
             }
         })
 
@@ -86,7 +87,8 @@ router.post("/login", async (req, res) => {
                 id: user._id,
                 username: user.username,
                 email:user.email,
-                profileImage: user.profileImage
+                profileImage: user.profileImage,
+                createdAt: user.createdAt,
             }
         })
           
