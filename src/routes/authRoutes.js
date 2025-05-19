2f0b8092ce136d7429a3acb7cd607ba2f48392f4
nicopys42 <123968243+nicopys42@users.noreply.github.com>
2025-05-19 13:16:46 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -37,7 +37,8 @@ router.post("/register", async (req, res) => {
         }
 
         //get random avatar
-        const profileImage = `https://api.dicebear.com/9.x/avataaars/svg?seed=${username}`
+        const profileImage = `https://api.dicebear.com/9.x/avataaars/svg?seed=${username}`\
+        console.log("Profile Image", profileImage);
 
         const user = new User({
             email,
